2b7b0c8b42f2a6e147f13f36398e6cbe5887044e
Abdul Hanan Nawaz <hanannawaz0@gmail.com>
2026-05-22 20:01:54 +0200

    feat: split data into train and test

---
+++ src/data/split_data.py
@@ -0,0 +1,16 @@
+from sklearn.model_selection import train_test_split
+
+def split_data(df, test_size=0.2, random_state=42):
+
+    X = df["text"]
+    y = df["label"]
+
+    X_train, X_test, y_train, y_test = train_test_split(
+        X,
+        y,
+        test_size=test_size,
+        random_state=random_state,
+        stratify=y
+    )
+
+    return X_train, X_test, y_train, y_test

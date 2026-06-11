44788005fcc4c7b32e39b993dfa8bdc46481941f
Abdul Hanan Nawaz <hanannawaz0@gmail.com>
2026-06-11 15:47:26 +0200

    feat: update the path

--- main.py
+++ main.py
@@ -1,9 +1,9 @@
 from scripts.train_classical import main as main_classical
-from scripts.train_bert import main as main_bert
+from scripts.train_distilbert_embeddings import main as main_distilbert
 
 def main():
     main_classical()
-    main_bert()
+    main_distilbert()
 
 
 if __name__ == "__main__":

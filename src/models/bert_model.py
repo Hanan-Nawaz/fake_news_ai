f5b28f76275809e05e0b2459860608da040fc9ce
Abdul Hanan Nawaz <hanannawaz0@gmail.com>
2026-06-11 14:36:08 +0200

    feat: update bert

--- src/models/bert_model.py
+++ src/models/bert_model.py
@@ -1,7 +1,7 @@
 from transformers import BertForSequenceClassification
 
 
-def get_bert_model(model_name="bert-base-uncased", num_labels=3):
+def get_bert_model(model_name="distilbert-base-uncased", num_labels=3):
     model = BertForSequenceClassification.from_pretrained(
         model_name,
         num_labels=num_labels
